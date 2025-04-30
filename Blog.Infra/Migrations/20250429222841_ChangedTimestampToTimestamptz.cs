01172dcf6e9a6a5b01532a63f305c5ae18ed1817
vinibressanini <vinimarcus41@gmail.com>
2025-04-30 10:40:31 -0300

    fix: database updated v1.3

---
+++ Blog.Infra/Migrations/20250429222841_ChangedTimestampToTimestamptz.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedTimestampToTimestamptz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "updated_at",
+                table: "tb_post",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "publish_at",
+                table: "tb_post",
+                type: "timestamptz",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "tb_post",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "processed_at",
+                table: "tb_domain_event",
+                type: "timestamptz",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "tb_domain_event",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "tb_comment",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "updated_at",
+                table: "tb_post",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "publish_at",
+                table: "tb_post",
+                type: "timestamp",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "tb_post",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "processed_at",
+                table: "tb_domain_event",
+                type: "timestamp",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "tb_domain_event",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "tb_comment",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+        }
+    }
+}
